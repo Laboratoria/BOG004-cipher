4d1c5c45ce61e3306583102f575f61aaeff1b015
Dan13l4 <kdbaron426@gmail.com>
2022-01-17 11:37:50 -0500

    encode fuction

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
 const cipher = {
-  // ...
+  enconde : function () {
+    let string = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let offset = 33;
+    let resultado = (offset + string) % 26;
+    console.log(string[resultado]);
+  }
+
+  
 };
 
 export default cipher;
