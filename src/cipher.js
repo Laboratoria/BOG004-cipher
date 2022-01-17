dc44bfc4dfafed083e2eed6fefe3f638e9c4c261
Dan13l4 <kdbaron426@gmail.com>
2022-01-17 12:33:35 -0500

    decode function

--- src/cipher.js
+++ src/cipher.js
@@ -13,6 +13,19 @@ const cipher = {
     return newWord;
   }
 
+  decode : function(offset, string){
+    let arrayFrase = [];
+    let newWord;
+    for(let i = 0; i < string.length; i++){
+      let numberAscii = string.charCodeAt(i);
+      let formula = ((numberAscii - 65 - offset)% 26) + 65;
+      let newWord = String.fromCharCode(formula);
+      arrayFrase.push(newWord);
+      newWord = arrayFrase.join("");
+    }
+    return newWord;
+  }
+
 };
 
 
