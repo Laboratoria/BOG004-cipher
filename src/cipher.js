d68a13b44166c5c327daa2d1782956175a264f2a
Dan13l4 <kdbaron426@gmail.com>
2022-01-17 12:24:47 -0500

    Encode fuction complete

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,18 @@
 const cipher = {
 
-  //primera prueba
-  //enconde : function () {
-    //let string = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    //let offset = 33;
-    //let resultado = (offset + string) % 26;
-    //console.log(string[resultado]);
-  //}
-
-  enconde : function (){
-    let string = "Poyo";
-    let offset = 4;
-    let nuevaPalabra;
-    for(let i = 0; i < string.length; i++) {
+  encode : function(offset, string){
+    let arrayFrase = [];
+    let newWord;
+    for(let i = 0; i < string.length; i++){
       let numberAscii = string.charCodeAt(i);
-      let problem = ((numberAscii - 65 + offset)% 26) + 65;
-      nuevaPalabra =  String.fromCharCode(problem);
+      let formula = ((numberAscii - 65 + offset)% 26) + 65;
+      let newWord = String.fromCharCode(formula);
+      arrayFrase.push(newWord);
+      newWord = arrayFrase.join("");
     }
-    
-    return nuevaPalabra;
-
+    return newWord;
   }
 
-  
 };
 
 
