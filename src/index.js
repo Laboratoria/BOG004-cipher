b1fe021bd86a459d15801e41486fc47032bac79b
Dan13l4 <kdbaron426@gmail.com>
2022-01-17 20:28:57 -0500

    adding instructions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 document.getElementById("instrucciones").addEventListener("click", ()=>{
-    window.alert("Holi");
+    window.alert("Bienvend@, porfavor lee con atencion las siguientes instrucciones.  \nCIFRADOR:   \n1. En mayusculas porfavor ingresa el texto que quiera cifrar, trata de no usar caracteres especiales como la coma, tilde o la letra ñ \n2. Escoge el numero de pasos que quieres que se corra la letra en el abecedario. \n3. Pulsa el boton -cifrar- para ver el resultado. \nDECIFRADOR: \n1.En mayusculas porfavor ingresa el texto que quiera Decifrar \n2. Escoge el numero de pasos que se usaron para cifrar la palabra \n3. Pulsa el boton -descifrar- para ver el resultado.");
   });
 
   document.getElementById("button").addEventListener("click", ()=>{
